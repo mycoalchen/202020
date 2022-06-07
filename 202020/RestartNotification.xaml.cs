8a02ba5a04fbcff037624594e75d4b2f12fe3637
Michael Chen <chen.michael.1116@gmail.com>
2022-06-07 19:10:24 -0400

    restart notification on wake from sleep - added file

---
+++ 202020/RestartNotification.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace _202020
+{
+    /// <summary>
+    /// Interaction logic for RestartNotification.xaml
+    /// </summary>
+    public partial class RestartNotification : Window
+    {
+        public RestartNotification()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
